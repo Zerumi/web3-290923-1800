8ca061cb0f33ac3d2446664a97cce8296330707a
Zerumi <46845708+Zerumi@users.noreply.github.com>
2023-10-09 02:52:20 +0300

    feat: actual graph equation

--- src/main/java/io/github/web32909231800/model/AreaResultChecker.java
+++ src/main/java/io/github/web32909231800/model/AreaResultChecker.java
@@ -2,6 +2,23 @@ package io.github.web32909231800.model;
 
 public class AreaResultChecker {
     public static boolean getResult(double x, double y, double r) {
+        // check 1st square -- 1/4 circle
+        if (x >= 0 && y >= 0) {
+            if (Math.pow(x, 2) + Math.pow(y, 2) <= Math.pow(r, 2))
+                return true;
+        }
+        // check 2nd square -- square
+        if (x <= 0 && y >= 0) {
+            if (-x <= r && y <= r)
+                return true;
+        }
+        // check 3rd square -- triangle
+        if (x <= 0 && y <= 0) {
+            if (-x - 2*y <= r)
+                return true;
+        }
+        // check 4th square -- always false
+        // also if all checks above fails...
         return false;
     }
 }
--- src/main/webapp/resources/js/graph.js
+++ src/main/webapp/resources/js/graph.js
@@ -27,37 +27,22 @@ calculator.setDefaultState(newDefaultState);
 function drawGraphByR(r) {
     calculator.setExpression({
         id: '1',
-        latex: '\\left(\\frac{x}{r}\\right)^{2}\\cdot\\sqrt{\\frac{\\left|\\left(\\frac{\\left|x\\right|}{\\frac{r}{7}}-3\\right)\\right|}{\\left(\\frac{\\left|x\\right|}{\\frac{r}{7}}-3\\right)}}+\\left(\\frac{\\frac{y}{\\frac{r}{7}}}{3}\\right)^{2}\\cdot\\sqrt{\\frac{\\left|\\frac{y}{\\frac{r}{7}}+\\frac{3}{7}\\cdot\\sqrt{33}\\right|}{\\left(\\frac{y}{\\frac{r}{7}}+\\frac{3}{7}\\cdot\\sqrt{33}\\right)}}-1\\le0\\ \\left\\{\\frac{y}{\\frac{r}{7}}\\ge0\\right\\}',
+        latex: 'x^{2}+y^{2}\\le r^{2}\\ \\left\\{x\\ge0\\right\\}\\left\\{y\\ge0\\right\\}',
         color: Desmos.Colors.ORANGE
     });
     calculator.setExpression({
         id: '2',
-        latex: '\\left(\\frac{x}{r}\\right)^{2}\\cdot\\sqrt{\\frac{\\left|\\left(\\frac{\\left|x\\right|}{\\frac{r}{7}}-4\\right)\\right|}{\\left(\\frac{\\left|x\\right|}{\\frac{r}{7}}-4\\right)}}+\\left(\\frac{\\frac{y}{\\frac{r}{7}}}{3}\\right)^{2}\\cdot\\sqrt{\\frac{\\left|\\frac{y}{\\frac{r}{7}}+\\frac{3}{7}\\cdot\\sqrt{33}\\right|}{\\left(\\frac{y}{\\frac{r}{7}}+\\frac{3}{7}\\cdot\\sqrt{33}\\right)}}-1\\le0\\ \\left\\{\\frac{y}{\\frac{r}{7}}\\le0\\right\\}\\ ',
+        latex: '-x\\le r\\ \\left\\{x\\le0\\right\\}\\ \\left\\{r\\ge y\\ge0\\right\\}',
         color: Desmos.Colors.ORANGE
     });
     calculator.setExpression({
         id: '3',
-        latex: '\\left|\\frac{\\frac{x}{\\frac{r}{7}}}{2}\\right|-(3\\cdot\\sqrt{33}-7)\\cdot\\frac{\\left(\\frac{x}{\\frac{r}{7}}\\right)^{2}}{112}-3+\\sqrt{1-\\left(\\left|\\left(\\frac{\\left|x\\right|}{\\frac{r}{7}}-2\\right)\\right|-1)^{2}\\right)}-\\frac{y}{\\frac{r}{7}}\\le0\\ \\left\\{\\frac{y}{\\frac{r}{7}}\\ \\le0\\right\\}',
+        latex: 'y\\ =\\ r\\ \\left\\{r\\ge-x\\ge0\\right\\}',
         color: Desmos.Colors.ORANGE
     });
     calculator.setExpression({
         id: '4',
-        latex: '9-\\frac{8\\left|x\\right|}{\\frac{r}{7}}\\ge\\frac{y}{\\frac{r}{7}}\\ \\left\\{0.75\\le\\frac{\\left|x\\right|}{\\frac{r}{7}}\\le1\\right\\}\\ \\left\\{0\\le\\frac{y}{\\frac{r}{7}}\\le3\\right\\}',
-        color: Desmos.Colors.ORANGE
-    });
-    calculator.setExpression({
-        id: '5',
-        latex: '\\frac{3\\left|x\\right|}{\\frac{r}{7}}+0.75\\ \\ge\\frac{y}{\\frac{r}{7}}\\ \\left\\{0.75\\ge\\frac{\\left|x\\right|}{\\frac{r}{7}}\\ge0.5\\right\\}\\ \\left\\{\\frac{y}{\\frac{r}{7}}\\ \\ge0\\right\\}',
-        color: Desmos.Colors.ORANGE
-    });
-    calculator.setExpression({
-        id: '6',
-        latex: '\\frac{y}{\\frac{r}{7}}\\ \\le\\ 2.25\\ \\left\\{\\frac{x}{\\frac{r}{7}}\\le0.5\\right\\}\\ \\left\\{\\frac{x}{\\frac{r}{7}}\\ge-0.5\\right\\}\\ \\left\\{\\frac{y}{\\frac{r}{7}}\\ge0\\right\\}',
-        color: Desmos.Colors.ORANGE
-    });
-    calculator.setExpression({
-        id: '7',
-        latex: '6\\cdot\\frac{\\sqrt{10}}{7}+\\left(1.5-\\frac{0.5\\left|x\\right|}{\\frac{r}{7}}\\right)\\cdot\\sqrt{\\frac{\\left|\\left(\\frac{\\left|x\\right|}{\\frac{r}{7}}-1\\right)\\right|}{\\frac{\\left|x\\right|}{\\frac{r}{7}}-1}}-6\\cdot\\frac{\\sqrt{10}}{14}\\cdot\\sqrt{\\left(4-\\left(\\frac{\\left|x\\right|}{\\frac{r}{7}}-1\\right)^{2}\\right)}\\ge\\frac{y}{\\frac{r}{7}}\\ \\left\\{\\frac{y}{\\frac{r}{7}}\\ \\ge0\\right\\}',
+        latex: '-x-2y\\le r\\ \\left\\{x\\le0\\right\\}\\left\\{y\\le0\\right\\}',
         color: Desmos.Colors.ORANGE
     });
     calculator.setExpression({id: '8', latex: 'r=' + r, lineOpacity: 0});
